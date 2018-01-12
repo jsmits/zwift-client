5c1467a6c5ce4e8442aac15d5cc85adaf387ce29
Sander Smits <sander@aidence.com>
2018-01-12 21:14:22 +0100

    Implement player status.

--- zwift/request.py
+++ zwift/request.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
 
+from .error import RequestException
+
 
 class Request:
     """Class for handling requests."""
@@ -20,4 +22,13 @@ class Request:
     def json(self, url):
         headers = self.get_headers(accept_type='application/json')
         resp = requests.get('https://us-or-rly101.zwift.com' + url, headers=headers)
+        if not resp.ok:
+            raise RequestException("{} - {}".format(resp.status_code, resp.reason))
         return resp.json()
+
+    def protobuf(self, url):
+        headers = self.get_headers(accept_type='application/x-protobuf-lite')
+        resp = requests.get('https://us-or-rly101.zwift.com' + url, headers=headers)
+        if not resp.ok:
+            raise RequestException("{} - {}".format(resp.status_code, resp.reason))
+        return resp.content
