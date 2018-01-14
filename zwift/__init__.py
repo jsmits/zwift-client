0ba2334ccdf11fb2870ffc5f31d8e3d8f78b5aee
Sander Smits <sander@aidence.com>
2018-01-14 16:49:24 +0100

    Fix for zwift import by setup.py.

--- zwift/__init__.py
+++ zwift/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """Top-level package for Zwift Mobile API client."""
-from .client import Client
-
+try:
+    from .client import Client
+except ImportError:
+    pass
+else:
+    __all__ = [Client]
 
 __author__ = """Sander Smits"""
 __email__ = 'jhmsmits@gmail.com'
 __version__ = '0.1.1.dev0'
-
-__all__ = [Client]
