88f6af1e3e23979443b7d988db1002c43db6c405
Sander Smits <sander@aidence.com>
2018-01-14 11:22:45 +0100

    Implement missing Profile methods.

--- zwift/profile.py
+++ zwift/profile.py
@@ -7,5 +7,26 @@ class Profile:
         self.player_id = player_id
         self.request = Request(get_access_token)
 
+    @property
     def profile(self):
         return self.request.json('/api/profiles/{}'.format(self.player_id))
+
+    @property
+    def followers(self):
+        return self.request.json(
+            '/api/profiles/{}/followers'.format(self.player_id))
+
+    @property
+    def followees(self):
+        return self.request.json(
+            '/api/profiles/{}/followees'.format(self.player_id))
+
+    def get_activities(self, start=0, limit=10):
+        return self.request.json(
+            '/api/profiles/{}/activities?start={}&limit={}'.format(
+                self.player_id, start, limit))
+
+    @property
+    def latest_activity(self):
+        activities = self.get_activities(0, 1)
+        return activities[0] if len(activities) == 1 else None
