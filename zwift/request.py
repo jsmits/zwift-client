ce0c961be11b13f517a2cdee67e8045b428e6c65
Sander Smits <sander@aidence.com>
2018-01-14 11:20:48 +0100

    PEP8.

--- zwift/request.py
+++ zwift/request.py
@@ -6,7 +6,9 @@ from .error import RequestException
 
 class Request:
     """Class for handling requests."""
-    DEFAULT_HEADERS = {"User-Agent": "Zwift/115 CFNetwork/758.0.2 Darwin/15.0.0"}
+    BASE_URL = 'https://us-or-rly101.zwift.com'
+    DEFAULT_HEADERS = {
+        "User-Agent": "Zwift/115 CFNetwork/758.0.2 Darwin/15.0.0"}
 
     def __init__(self, get_access_token):
         self.get_access_token = get_access_token
@@ -21,14 +23,16 @@ class Request:
 
     def json(self, url):
         headers = self.get_headers(accept_type='application/json')
-        resp = requests.get('https://us-or-rly101.zwift.com' + url, headers=headers)
+        resp = requests.get(self.BASE_URL + url, headers=headers)
         if not resp.ok:
-            raise RequestException("{} - {}".format(resp.status_code, resp.reason))
+            raise RequestException("{} - {}".format(
+                resp.status_code, resp.reason))
         return resp.json()
 
     def protobuf(self, url):
         headers = self.get_headers(accept_type='application/x-protobuf-lite')
-        resp = requests.get('https://us-or-rly101.zwift.com' + url, headers=headers)
+        resp = requests.get(self.BASE_URL + url, headers=headers)
         if not resp.ok:
-            raise RequestException("{} - {}".format(resp.status_code, resp.reason))
+            raise RequestException("{} - {}".format(
+                resp.status_code, resp.reason))
         return resp.content
